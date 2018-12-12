efa1c47cf21f173e9d21ca5d06bb7a77c5d40445
shaktiman1110 <45808829+shaktiman1110@users.noreply.github.com>
2018-12-12 12:41:44 +0530

    Update MapExample.java

--- MapExample.java
+++ MapExample.java
@@ -4,41 +4,18 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapExample {
+public class ForEachDemo {
 public static void main(String[] args) {
-	List<String> uris = new ArrayList<>();
-	uris.add("c://myfile1.txt");
-	uris.add("c://myfile2.txt");
-	uris.add("c://myfile3.txt");
-	uris.add("c://myfile4.txt");
 	
-	uris.stream()
-			.map(mapper->Paths.get(mapper))
-			.forEach(System.out::println);
-	
-	System.out.println("-----------------------------");
-
 	List<Detail> details= new ArrayList<>();
 	
 	List<String> part1= new ArrayList<>();
-	part1.add("part1_1");
-	part1.add("part1_2");
-	part1.add("part1_3");
-	part1.add("part1_4");
+	part1.add("This is ");
+	part1.add("demo for ");
+	part1.add("Default method");
+	part1.add("I am not disturbing any other method of List interface which were there before java 8");
 	System.out.println("demo for forEach");
 	part1.forEach(System.out::println);
-	
-	List<String> part2= new ArrayList<>();
-	part2.add("part2_1");
-	part2.add("part2_2");
-	part2.add("part2_3");
-	part2.add("part2_4");
 
-	details.add(new Detail(1001, part1));
-	details.add(new Detail(1002, part2));
-	
-	details.stream()
-	.flatMap(mapper->mapper.getParts().stream())
-	.forEach(System.out::println);
 }
 }
