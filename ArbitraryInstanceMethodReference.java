87dd114cbf5c2148d227867ae8cc65f4d6df79e5
Nishant <45808829+shaktiman1110@users.noreply.github.com>
2018-12-19 10:24:51 +0530

    Add files via upload
    Method reference to instance method from class type – Class::instanceMethodName

---
+++ ArbitraryInstanceMethodReference.java
@@ -0,0 +1,27 @@
+package methodReference;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArbitraryInstanceMethodReference {
+    public static void main(String args[]) {
+        final List<Person> people = Arrays.asList(new Person("dan"), new Person("laura"));
+        // Method reference
+        people.forEach(Person::printName);
+        // Lambda expression
+        people.forEach(person -> person.printName());
+        // normal
+        for (final Person person : people) {
+            person.printName();
+        }
+    }
+    private static class Person {
+        private String name;
+        public Person(final String name) {
+            this.name = name;
+        }
+        public void printName() {
+            System.out.println(name);
+        }
+    }
+}
